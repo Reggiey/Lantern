0939fe7a81d4462f45ed6da6748bbf46d33f6f3a
Reggie <reggiey@163.com>
2024-10-29 14:24:31 +0800

    提交

--- Package.swift
+++ Package.swift
@@ -1,17 +1,25 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "FSPagerView",
-    platforms: [
-        .iOS(.v9)
-    ],
+    name: "Lantern",
     products: [
-        .library(name: "FSPagerView", targets: ["FSPagerView"]),
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Lantern",
+            targets: ["Lantern"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "FSPagerView", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Lantern",
+            dependencies: []),
     ]
 )
